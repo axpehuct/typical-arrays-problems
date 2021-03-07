43ab8ae96d54986a2b9105e2591904bf46e4b40d
Equento <axpehuct95@gmail.com>
2021-03-07 19:02:33 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array) {     
+  if(array === undefined){
+    return 0;
+  }
+  if(!array.length){
+    return 0;
+  }
+  let min = array[0];
+  array.forEach(element => { 
+    if(element < min){
+      min = element;
+    }
+  });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined){
+    return 0;
+  }
+  if(!array.length){
+    return 0;
+  }
+  let max = array[0];
+  array.forEach(element => {
+    if(element > max){
+      max = element;
+    }
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array.length < 1 || !array)
+    return 0;
+  if(array === undefined){
+    return 0;
+  }
+  // if(!array.length){
+  //   return 0;
+  // }
+  let sum = 0;
+  for(let i = 0; i < array.length; ++i){
+    sum += array[i];
+  } 
+  return sum / array.length;
 }
